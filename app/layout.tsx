74c6f9169ac84fa23f75de252c4e59a6f8cceb1f
KayGV25 <khuongvudang25@gmail.com>
2026-04-22 08:57:04 +0700

    no scrolling 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex flex-col overflow-y-hidden">
+      <body className="flex flex-col overflow-y-hidden overscroll-none">
         {children}
         <Analytics/>
       </body>
